fff52afb7eb3c546857a92d7f669944817b45664
farhan <farhnn.r@gmail.com>
2022-05-17 15:31:48 +0700

    install, configuration react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./pages/App";
+import Routes from "./routes/Routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>
 );
 
