96f87cb5e8dc14e21e9e4370386dd258fde161bf
farhan <farhnn.r@gmail.com>
2022-05-17 17:25:49 +0700

    add nav brand and burgger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Burger } from "@mantine/core";
 
 const Navbar = () => {
+  const [opened, setOpened] = useState(false);
+  const title = opened ? "Close navigation" : "Open navigation";
+
   return (
-    <div className="bg-cyan-500 py-5">
-      <div className="container mx-auto">Navbar</div>
+    <div className="bg-cyan-500 py-3">
+      <div className="px-4 lg:px-0 lg:container mx-auto text-neutral-100">
+        <div className="flex justify-between">
+          <div className="font-normal">
+            BEBECE
+            <br />
+            <span className="font-bold text-xl">NEWS</span>
+          </div>
+
+          <div className="self-center block lg:hidden">
+            <Burger color="white" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
