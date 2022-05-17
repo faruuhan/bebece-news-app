5595eaa7a29defb492305ae3e5c47b037b404f22
farhan <farhnn.r@gmail.com>
2022-05-17 19:18:59 +0700

    nav menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,17 +6,41 @@ const Navbar = () => {
   const title = opened ? "Close navigation" : "Open navigation";
 
   return (
-    <div className="bg-cyan-500 py-3">
-      <div className="px-4 lg:px-0 lg:container mx-auto text-neutral-100">
-        <div className="flex justify-between">
+    <div className="bg-teal-600 py-3">
+      <div className="px-4 flex items-center lg:px-0 lg:container mx-auto text-teal-200">
+        <div className="flex items-center justify-between pr-4">
           <div className="font-normal">
             BEBECE
             <br />
             <span className="font-bold text-xl">NEWS</span>
           </div>
+          <div className="">
+            <Burger className="block lg:hidden" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
+          </div>
+        </div>
 
-          <div className="self-center block lg:hidden">
-            <Burger color="white" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
+        <div className="flex justify-between w-full">
+          <div className="flex">
+            <a href="#" className="block px-4 hover:text-white">
+              Bisnis
+            </a>
+            <a href="#" className="block px-4 hover:text-white">
+              Hiburan
+            </a>
+            <a href="#" className="block px-4 hover:text-white">
+              Kesehatan
+            </a>
+            <a href="#" className="block px-4 hover:text-white">
+              Sains
+            </a>
+            <a href="#" className="block px-4 hover:text-white">
+              Teknologi
+            </a>
+          </div>
+          <div className="flex">
+            <a href="#" className="block px-4 hover:text-white">
+              Login
+            </a>
           </div>
         </div>
       </div>
