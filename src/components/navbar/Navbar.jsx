dfce9bfc7ec128f0d242fd5692a8dccd792ee435
farhan <farhnn.r@gmail.com>
2022-05-17 20:16:25 +0700

    navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,40 +6,42 @@ const Navbar = () => {
   const title = opened ? "Close navigation" : "Open navigation";
 
   return (
-    <div className="bg-teal-600 py-3">
-      <div className="px-4 flex items-center lg:px-0 lg:container mx-auto text-teal-200">
-        <div className="flex items-center justify-between pr-4">
+    <div className="bg-teal-600 lg:py-3">
+      <div className="flex flex-col lg:items-center lg:px-0 lg:container mx-auto text-teal-200 lg:flex-row">
+        <div className="flex items-center justify-between px-4 py-4 border-b border-teal-500 lg:px-0 lg:py-0 lg:border-b-0">
           <div className="font-normal">
             BEBECE
             <br />
             <span className="font-bold text-xl">NEWS</span>
           </div>
           <div className="">
-            <Burger className="block lg:hidden" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
+            <Burger color="#99f6e4" className="block lg:hidden" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
           </div>
         </div>
 
-        <div className="flex justify-between w-full">
-          <div className="flex">
-            <a href="#" className="block px-4 hover:text-white">
+        <div className={`${opened ? "block" : "hidden"} lg:flex flex-col justify-between w-full lg:flex-row`}>
+          <div className="flex flex-col lg:flex-row">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
               Bisnis
             </a>
-            <a href="#" className="block px-4 hover:text-white">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
               Hiburan
             </a>
-            <a href="#" className="block px-4 hover:text-white">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
               Kesehatan
             </a>
-            <a href="#" className="block px-4 hover:text-white">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
               Sains
             </a>
-            <a href="#" className="block px-4 hover:text-white">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
               Teknologi
             </a>
           </div>
-          <div className="flex">
-            <a href="#" className="block px-4 hover:text-white">
-              Login
+          <div className="flex flex-col lg:flex-row">
+            <a href="#" className="block px-4 py-3 lg:py-0 hover:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+              </svg>
             </a>
           </div>
         </div>
