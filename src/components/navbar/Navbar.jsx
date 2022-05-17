5247068e192cf7f10a36b80d4aa3cc1a16472a33
farhan <farhnn.r@gmail.com>
2022-05-17 16:57:46 +0700

    add navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-cyan-500 py-5">
+      <div className="container mx-auto">Navbar</div>
+    </div>
+  );
+};
+
+export default Navbar;
