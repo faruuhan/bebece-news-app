4db775a49bd2fc742f4c9ab7285b7754f5780848
farhan <farhnn.r@gmail.com>
2022-05-18 07:46:25 +0700

    add component card news vertical

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import CardNewsVertical from "../../components/cardnewsvertical/CardNewsVertical";
 
 const Homepage = () => {
-  return <Layout>Homepage</Layout>;
+  return (
+    <Layout>
+      <div className="lg:container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-2">
+          <div className="w-full lg:w-4/12">
+            <CardNewsVertical />
+          </div>
+          <div className="w-full lg:w-4/12">
+            <CardNewsVertical />
+          </div>
+          <div className="w-full lg:w-4/12">
+            <CardNewsVertical />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Homepage;
