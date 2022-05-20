7fdedfa3d237993d3eed8be5c3909fc968f4eff9
farhan <farhnn.r@gmail.com>
2022-05-20 17:12:19 +0700

    consume api on categories page

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import Layout from "../../components/layout/Layout";
 
 const Categories = () => {
   const { categories } = useParams();
+
+  useEffect(() => {
+    fecthNewsCategories();
+  }, []);
+
+  const fecthNewsCategories = async () => {
+    await axios
+      .get(`https://newsapi.org/v2/top-headlines?country=us&category=${categories}&apiKey=${process.env.REACT_APP_API_KEY}`)
+      .then((ress) => {
+        console.log(ress.data.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return <Layout>{categories}</Layout>;
 };
 
